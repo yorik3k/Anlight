a8729fa4bde26837b214bcfca993169c9c8fdc3e
yorik3k <yorik3k@yandex.com>
2026-07-25 10:40:59 +0300

    Migrate project to UE 5.7.4

--- Source/gamededPrj.Target.cs
+++ Source/gamededPrj.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class gamededPrjTarget : TargetRules
 {
-	public gamededPrjTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
-		ExtraModuleNames.Add("gamededPrj");
-	}
+    public gamededPrjTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.V6;  // ← Меняем с V5 на V6
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;  // ← Меняем с Unreal5_4 на Unreal5_7
+        ExtraModuleNames.Add("gamededPrj");
+    }
 }
--- Source/gamededPrjEditor.Target.cs
+++ Source/gamededPrjEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class gamededPrjEditorTarget : TargetRules
 {
-	public gamededPrjEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
-		ExtraModuleNames.Add("gamededPrj");
-	}
+    public gamededPrjEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V6;  // ← Меняем с V5 на V6
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;  // ← Меняем с Unreal5_4 на Unreal5_7
+        ExtraModuleNames.Add("gamededPrj");
+    }
 }
